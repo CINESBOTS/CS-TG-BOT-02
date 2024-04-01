493c4b5e72bf73d76e3586913b813ecf70381165
fbleech <100531321+fbleech@users.noreply.github.com>
2024-04-02 01:20:32 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -29,7 +29,7 @@ async def channel_post(client: Client, message: Message):
 reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
 await reply_text.edit(f"<b> Here is your link </b>\n\n{link}\n\nClick the button below to share:", reply_markup=reply_markup, disable_web_page_preview=True)
 
-    if not DISABLE_CHANNEL_BUTTON:
+if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
 
 @Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID))
