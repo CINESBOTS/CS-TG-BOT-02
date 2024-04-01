47e7551f616998971033945d22270fa5f63b092c
fbleech <100531321+fbleech@users.noreply.github.com>
2024-04-02 02:30:36 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -31,7 +31,7 @@ async def channel_post(client: Client, message: Message):
     await reply_text.edit(f"<b> Here is your link </b>\n\n {link}", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
-        await reply_text.reply(f"Here is your link: {link}")
+        await post_message.edit_reply_markup(reply_markup)
 
 @Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID))
 async def new_post(client: Client, message: Message):
@@ -45,7 +45,7 @@ async def new_post(client: Client, message: Message):
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     try:
-        await message.edit_reply_markup(reply_markup)
+        await reply_text.reply(f"Here is your link: {link}")
     except Exception as e:
         print(e)
         pass
